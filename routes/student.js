1df66bbb46f5f642551d4ef6309f38348585c70e
rajabinekoo <ali.rajabinekoo@gmail.com>
2023-04-13 12:23:58 +0330

    fixed

--- routes/student.js
+++ routes/student.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const { Student } = require("../database/models/student");
 // const { AppError } = require("../utils/types");
 const { requestHandler } = require("../services/request.handler");
-const { addStudentController } = require("../controller/student");
+const { addStudentController } = require("../controllers/student");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
